b325d0d3639c75abe86292b95983554757841ed6
Najmus Sakib <nazmusakib412@gmail.com>
2023-11-24 18:59:26 +0600

    build: add sass support

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-export default function Home() {
+import Home from '@/modules/Home/Home';
+
+export default function HomePage() {
   return (
     <main>
       <h1 className="h1">Binary SUST</h1>
       <p>Technology for real life</p>
+      <Home />
       <button className="bg-sky-500 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
         Click me
       </button>
