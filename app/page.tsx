bd3a18692ec486708a60a092c53a0aba8ecc7196
Najmus Sakib <nazmusakib412@gmail.com>
2023-11-24 18:38:00 +0600

    build: add prettier-plugin-tailwindcss plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1>Binary SUST</h1>
+      <h1 className="h1">Binary SUST</h1>
       <p>Technology for real life</p>
+      <button className="bg-sky-500 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Click me
+      </button>
     </main>
-  )
+  );
 }
