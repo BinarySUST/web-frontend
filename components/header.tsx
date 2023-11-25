6e86191d8633c33e3b6e0cbfbbaf7b9254b36181
heronet <sirat4757@gmail.com>
2023-11-26 01:54:35 +0600

    add header

---
+++ components/header.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from './ui/navigation-menu';
+
+export default function Header() {
+  return (
+    <div className="flex justify-between border-b p-2">
+      <div className="flex items-center">
+        <h1 className="px-1 text-lg">
+          <Link href="/">BinarySUST</Link>
+        </h1>
+      </div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/login" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Login
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/register" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Register
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
