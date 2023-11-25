c16a5648de94adc431abac0d1d227c7818aa50e3
heronet <sirat4757@gmail.com>
2023-11-26 01:54:35 +0600

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import '@/styles/globals.scss';
+import Header from '@/components/header';
+import {cn} from '@/lib/utils';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -12,7 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('dark', inter.className)}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
