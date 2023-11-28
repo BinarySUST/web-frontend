acd3b1429f78c0a112c04485b3ffafc1c327dac0
heronet <sirat4757@gmail.com>
2023-11-28 14:36:22 +0600

    add register page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
-import Login from '@/modules/Auth/SignIn/Login';
+import SignIn from '@/modules/Auth/SignIn/SignIn';
 
 export default function LoginPage() {
   return (
     <div>
       <h1 className="p-2 text-center text-lg">Login to BinarySUST</h1>
       <div className="mx-auto w-full max-w-md">
-        <Login />
+        <SignIn />
       </div>
     </div>
   );
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,12 @@
+import SignUp from '@/modules/Auth/SignUp/SignUp';
+
 export default function RegisterPage() {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1 className="p-2 text-center text-lg">Register BinarySUST account</h1>
+      <div className="mx-auto w-full max-w-md">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
