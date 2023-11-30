4a99a0b846767f3804dd997a1d9f07293104dbdc
heronet <sirat4757@gmail.com>
2023-11-30 17:43:05 +0600

    refactor validator

--- modules/Auth/SignIn/SignIn.tsx
+++ modules/Auth/SignIn/SignIn.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {Button} from '@/components/ui/button';
 import {Form} from '@/components/ui/form';
-import {SignInSchemaType, signInFormSchema} from './validation/SignIn';
+import {SignInSchemaType, signInFormSchema} from './validator';
 import {zodResolver} from '@hookform/resolvers/zod';
 import {useForm} from 'react-hook-form';
 import InputField from './InputField';
